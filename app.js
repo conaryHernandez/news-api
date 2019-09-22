d7a67e5b52ad9c2d32118dcb405feb35ff596f7a
conaryh <conaryh@gmail.com>
2019-09-22 12:41:22 -0600

    allowing cors

--- app.js
+++ app.js
@@ -7,6 +7,13 @@ const feedRoutes = require('./routes/feed');
 
 app.use(bodyParser.json());
 
+// enabled CORS
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*'); // allowing specific origins
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE'); // allowing specific origins to use our methods
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // allowing specific origins to set headers
+});
+
 app.use('/feed', feedRoutes);
 
 app.listen(8080);
