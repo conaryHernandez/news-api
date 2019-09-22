8807a2beecf7bfab5f43aa58034b4aff3c554fb0
conaryh <conaryh@gmail.com>
2019-09-22 16:39:05 -0600

    added BE validations and database connection (mongoose)

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const MONGODB_URI = 'mongodb://conaryh:k9X9MpdWnfHYcqMC@cluster0-shard-00-00-nvbxl.mongodb.net:27017,cluster0-shard-00-01-nvbxl.mongodb.net:27017,cluster0-shard-00-02-nvbxl.mongodb.net:27017/messages?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&w=majority';
 
 const app = express();
 
@@ -7,13 +9,20 @@ const feedRoutes = require('./routes/feed');
 
 app.use(bodyParser.json());
 
-// enabled CORS
+// CORS enabled
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*'); // allowing specific origins
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE'); // allowing specific origins to use our methods
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // allowing specific origins to set headers
+    next();
 });
 
 app.use('/feed', feedRoutes);
 
-app.listen(8080);
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        app.listen(8080);
+    })
+    .catch(err => {
+        console.log(err);
+    });
--- controllers/feed.js
+++ controllers/feed.js
@@ -1,18 +1,41 @@
+const { validationResult } = require('express-validator');
+
 exports.getPosts = (req, res, next) => {
     res.status(200).json({
-        posts: [{ title: 'Post 1', content: 'This is the first post' }],
+        posts: [{
+            _id: 1,
+            title: 'Post 1',
+            content: 'This is the first post',
+            imageUrl: 'images/duck.jpg',
+            creator: {
+                name: 'Conary'
+            },
+            createdAt: new Date()
+        }],
     });
 };
 
 exports.createPost = (req, res, next) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        return res.status(422).json({
+            message: 'Validation Failed, entered date is incorrect',
+            errors: errors.array()
+        });
+    }
     const { title, content } = req.body;
 
     res.status(201).json({
         message: 'Post created successfully',
         post: {
-            id: new Date().toISOString(),
+            _id: new Date().toISOString(),
             title,
             content,
+            creator: {
+                name: 'Conary'
+            },
+            createdAt: new Date()
         },
     });
 };
