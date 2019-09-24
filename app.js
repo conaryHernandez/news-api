7e87dc79f0c2d38fceed3f1b61991a4844aefd7f
conaryh <conaryh@gmail.com>
2019-09-24 10:31:53 -0600

    updating and deleting post

--- app.js
+++ app.js
@@ -1,38 +1,51 @@
 const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const multer = require('multer');
-const uuidv4 = require('uuid/v4')
-
 // CONSTANTS
 const MONGODB_URI = 'mongodb://conaryh:k9X9MpdWnfHYcqMC@cluster0-shard-00-00-nvbxl.mongodb.net:27017,cluster0-shard-00-01-nvbxl.mongodb.net:27017,cluster0-shard-00-02-nvbxl.mongodb.net:27017/messages?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&w=majority';
 
 const app = express();
- 
-const storage = multer.diskStorage({
-    destination: function(req, file, cb) {
+const feedRoutes = require('./routes/feed');
+
+// const storage = multer.diskStorage({
+//     destination: function(req, file, cb) {
+//         cb(null, 'images');
+//     },
+//     filename: function(req, file, cb) {
+//         cb(null, uuidv4())
+//     }
+// });
+
+const fileStorage = multer.diskStorage({
+    destination: (req, file, cb) => {
         cb(null, 'images');
     },
-    filename: function(req, file, cb) {
-        cb(null, uuidv4())
+    filename: (req, file, cb) => {
+        cb(null, new Date().toISOString() + '-' + file.originalname);
     }
 });
 
+
 const fileFilter = (req, file, cb) => {
-    if (file.mimeType === 'image/png' || file.mimeType === 'image/jpg' || file.mimeType === 'image/jpeg') {
+    if (
+        file.mimetype === 'image/png' ||
+        file.mimetype === 'image/jpg' ||
+        file.mimetype === 'image/jpeg'
+    ) {
         cb(null, true);
     } else {
         cb(null, false);
     }
 };
 
-const feedRoutes = require('./routes/feed');
-
-app.use(bodyParser.json());
+app.use(bodyParser.json()); // application/json
 app.use(
-    multer({ storage, fileFilter: fileFilter }).single('image')
+    multer({ storage: fileStorage, fileFilter: fileFilter }).single('image')
 );
+
 app.use('/images', express.static(path.join(__dirname, 'images')));
 // CORS enabled
 app.use((req, res, next) => {
@@ -41,6 +54,7 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // allowing specific origins to set headers
     next();
 });
+
 
 app.use('/feed', feedRoutes);
 
--- controllers/feed.js
+++ controllers/feed.js
@@ -1,3 +1,6 @@
+const fs = require('fs');
+const path = require('path');
+
 const { validationResult } = require('express-validator');
 const Post = require('../models/post');
 
@@ -60,8 +63,6 @@ exports.createPost = (req, res, next) => {
 
         throw error;
     }
-
-    console.log('req', req.file);
     if (!req.file) {
         const error = new Error('No Image provided.');
 
@@ -70,9 +71,9 @@ exports.createPost = (req, res, next) => {
         throw error;
     }
 
-    const imageUrl = req.file.path.replace("\\" ,"/");
-    imageUrl = req.file.path.replace("\\","/");
-    // for mac const imageUrl = req.file.path;
+    // const imageUrl = req.file.path.replace("\\" ,"/");
+    // imageUrl = req.file.path.replace("\\","/");
+    const imageUrl = req.file.path;
     const { title, content } = req.body;
     const post = new Post({
         title,
@@ -98,4 +99,105 @@ exports.createPost = (req, res, next) => {
 
             next(err);
         })
+};
+
+exports.updatePost = (req, res, next) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        const error = new Error('Validation Failed, entered data is incorrect');
+
+        error.statusCode = 422;
+
+        throw error;
+    }
+    const { postId } = req.params;
+    const { title, content } = req.body;
+    let imageUrl = req.body.image;
+
+    if (req.file) {
+        imageUrl = req.file.path;
+    }
+
+    if (!imageUrl) {
+        const error = new Error('No Image provided.');
+
+        error.statusCode = 422;
+
+        throw error;
+    }
+
+    Post.findById(postId)
+        .then((post) => {
+            if (!post) {
+                const error = new Error('No post found.');
+
+                error.statusCode = 404;
+
+                throw error;
+            }
+
+            if (imageUrl !== post.imageUrl) {
+                clearImage(post.imageUrl);
+            }
+
+            post.title = title;
+            post.content = content;
+            post.imageUrl = imageUrl;
+
+            return post.save();
+        })
+        .then((result) => {
+            res
+                .status(200)
+                .json({
+                    message: 'Post Updated',
+                    post: result,
+                });
+        })
+        .catch(err => {
+            if (!err.statusCode) {
+                err.statusCode = 500;
+            }
+
+            next(err);
+        })
+};
+
+exports.deletePost = (req, res, next) => {
+    const { postId } = req.params;
+
+    Post.findById(postId)
+        .then(post => {
+            if (!post) {
+                const error = new Error('No post found.');
+
+                error.statusCode = 404;
+
+                throw error;
+            }
+            // check user
+            clearImage(post.imageUrl);
+
+            return Post.findByIdAndRemove(postId);
+        })
+        .then((result) => {
+            res
+                .status(200)
+                .json({
+                    message: 'Post Deleted',
+                });
+        }).catch(err => {
+            if (!err.statusCode) {
+                err.statusCode = 500;
+            }
+
+            next(err);
+        })
+}
+
+
+const clearImage = filePath => {
+    filePath = path.join(__dirname, '..', filePath);
+    fs.unlink(filePath, err => console.log(err));
 };
