c532bb832befb80ffb919a3135b41875bcaa8848
conaryh <conaryh@gmail.com>
2019-09-26 11:06:37 -0600

    added socket io server

--- app.js
+++ app.js
@@ -71,7 +71,11 @@ app.use((error, req, res, next) => {
 
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
-        app.listen(8080);
+        const server = app.listen(8080);
+        const io = require('socket.io')(server);
+        io.on('connection', socket => {
+            console.log('client connected');
+        })
     })
     .catch(err => {
         console.log(err);
